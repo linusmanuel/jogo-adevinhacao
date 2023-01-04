bdf8c33aebe6f5a585026b24c311fee7fce3f26a
Joao Lino <linusmannuel@gmail.com>
2023-01-04 21:02:04 +0100

    feat: Get user inputs

--- jogo.py
+++ jogo.py
@@ -1,1 +1,8 @@
+print('********************************')
 print('Bem-vindo, ao jogo de advinhação')
+print('********************************')
+
+numero_secreto = 42
+chute = input("Digite um numero: ")
+
+print("Você digitou ", chute)
