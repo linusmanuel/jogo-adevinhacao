07236c2d5aa0431502fa8d1e7e429e5245aeb8e8
Joao Lino <linusmannuel@gmail.com>
2023-01-08 09:53:33 +0100

    Add if and else struture

--- jogo.py
+++ jogo.py
@@ -8,5 +8,8 @@ chute = int(input("Digite um numero: "))
 if(numero_secreto == chute):
     print("Você digitou", chute, ", Você acertou!!!")
 else:
-    print('Você errou!')
+    if(chute > numero_secreto):
+        print('Você errou! Seu chute foi acima do número secreto')
+    else:
+        print('Você errou seu chute foi abaixo do número secreto')
 print('Fim do jogo!')
