018af70035e1d35999506e5b68aff4ab4061da10
Joao Lino <linusmannuel@gmail.com>
2023-01-10 21:10:34 +0100

    feat: Add while loop

--- jogo.py
+++ jogo.py
@@ -3,16 +3,19 @@ print('Bem-vindo, ao jogo de advinhação')
 print('********************************')
 
 numero_secreto = 42
-chute = int(input("Digite um numero: "))
+total_de_tentativas = 3
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+while(total_de_tentativas > 0):
+    chute = int(input("Digite um numero: "))
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Você digitou", chute, ", Você acertou!!!")
-elif(maior):
-    print('Você errou! Seu chute foi acima do número secreto')
-elif(menor):
-    print('Você errou seu chute foi abaixo do número secreto')
+    if(acertou):
+        print("Você digitou", chute, ", Você acertou!!!")
+    elif(maior):
+        print('Você errou! Seu chute foi acima do número secreto')
+    elif(menor):
+        print('Você errou seu chute foi abaixo do número secreto')
+    total_de_tentativas = total_de_tentativas - 1
 print('Fim do jogo!')
