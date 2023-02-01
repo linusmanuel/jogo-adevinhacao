ab5ec0d1ed3c4bd44109860ba02b1e71f8248d22
Joao Lino <linusmannuel@gmail.com>
2023-02-01 11:37:57 +0100

    feat: adiciona funcao escolhe jogo

--- jogos.py
+++ jogos.py
@@ -1,14 +1,17 @@
 import advinhacao
 import forca
+def escolhe_jogo():
+    print('********************************')
+    print('*****  Escolhe o seu Jogo  *****')
+    print('********************************')
 
-print('********************************')
-print('*****  Escolhe o seu Jogo  *****')
-print('********************************')
+    print('(1) Forca, (2) Advinhacação')
+    jogo = int(input('Qual jogo você quer? '))
 
-print('(1) Forca, (2) Advinhacação')
-jogo = int(input('Qual jogo você quer? '))
+    if (jogo == 1):
+        forca.jogar()
+    elif (jogo == 2):
+        advinhacao.jogar()
 
-if (jogo == 1):
-    forca.jogar()
-elif (jogo == 2):
-    advinhacao.jogar()
+if(__name__ == '__main__'):
+    escolhe_jogo()
