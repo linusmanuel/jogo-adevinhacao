9f2fed872c93cc18c16b8012cee5666bf9014f96
Joao Lino <linusmannuel@gmail.com>
2023-02-10 23:12:11 +0100

    removing spaces

--- forca.py
+++ forca.py
@@ -2,7 +2,6 @@ def jogar():
     print('********************************')
     print('Bem-vindo, ao jogo de Forca')
     print('********************************')
-    print('Fim do jogo!')
 
     palavra_secreta = 'banana'
     acertou = False
@@ -10,11 +9,13 @@ def jogar():
     index = 0
 
     while(not enforcou and not acertou):
-        chute = input('Qual letra: ')
+        chute = input('Qual letra: ').strip()
         for letra in palavra_secreta:
-            if(letra == chute):
+            if(letra.upper() == chute.upper()):
                 index += 1
                 print(f'Encontrei a letra {letra} na posição {index}')
+
+    print('Fim do jogo!')
 
 if(__name__ == '__main__'):
     jogar()
