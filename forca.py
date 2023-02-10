faf2b1353a960bb36252f50a1f6cf1424ee83ee9
Joao Lino <linusmannuel@gmail.com>
2023-02-10 22:35:51 +0100

    find letter position

--- forca.py
+++ forca.py
@@ -4,11 +4,17 @@ def jogar():
     print('********************************')
     print('Fim do jogo!')
 
+    palavra_secreta = 'banana'
     acertou = False
     enforcou = False
+    index = 0
 
     while(not enforcou and not acertou):
-        print('Jogando...')
+        chute = input('Qual letra: ')
+        for letra in palavra_secreta:
+            if(letra == chute):
+                index += 1
+                print(f'Encontrei a letra {letra} na posição {index}')
 
 if(__name__ == '__main__'):
     jogar()
